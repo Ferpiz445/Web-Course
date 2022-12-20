de87ff3c8741f444a91fcb2a8649b1d2c1d25d6e
Ferpiz445 <89951811+Ferpiz445@users.noreply.github.com>
2022-12-19 22:20:07 -0300

    obj, função e Var vs Let

---
+++ fundamentos/object.js
@@ -0,0 +1,23 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = '4998.90'
+prod1['Desconto legal'] = 0.40 // evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+    obj:{
+        blabla: 1,
+        obj:{
+            blabla: 2 // um objeto pode ter seus devidos childs
+        }
+    }
+}
+
+// transformando em json
+
+'{ "nome": "Camisa Polo", "preco": 79.90}'
+
+console.log(prod2)
---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,7 @@
+function teste(){
+    var local = 123
+    console.log(local)
+}
+
+teste()
+console.log(local)
