8a5987374e5bc8b67a08052d79110e1308f08c5f
Ferpiz445 <89951811+Ferpiz445@users.noreply.github.com>
2022-12-24 19:22:22 -0300

    começo de controles

---
+++ controle/if1.js
@@ -0,0 +1,10 @@
+function soBoaNoticia(nota){
+    if(nota >= 7){
+        console.log('Aprovado com ' + nota)
+    } else {
+        console.log('Reprovado com ' + nota)
+    }
+}
+
+soBoaNoticia(8.1)
+soBoaNoticia(6.1)
