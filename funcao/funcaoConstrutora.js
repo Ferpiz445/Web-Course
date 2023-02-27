a0c571a6acc0d618b5d4a604e02029bbff7f13e0
Felipe Monteiro <fe.monteiro2002@gmail.com>
2023-02-27 16:03:53 -0300

    função construtora

---
+++ funcao/funcaoConstrutora.js
@@ -0,0 +1,22 @@
+function Carro(veloMax = 200, delta = 5){
+    // atributo privado
+    let veloAtual = 0
+
+    // metodo publico
+    this.acelerar = function(){
+        if(veloAtual + delta <= veloMax){
+            veloAtual += delta
+        } else {
+            veloAtual = veloMax
+        }
+    }
+
+    // metodo publico
+    this.getVeloAtual = function(){
+        return veloAtual
+    }
+}
+
+const uno = new Carro
+uno.acelerar()
+console.log(uno.getVeloAtual())
